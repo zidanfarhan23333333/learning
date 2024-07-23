0d7ef4a95d184a69ddc3c0c18a94584e7bda37c6
unknown <sfarkhan48@gmail.com>
2024-07-23 12:12:17 +0700

    be

--- be/src/utils/connetDB.ts
+++ be/src/utils/connetDB.ts
@@ -1,1 +1,13 @@
 import mongoose from "mongoose";
+
+mongoose
+  .connect(
+    "mongodb+srv://farhan:<Farhan>@cluster0.t2nn1lv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("Mongodb connected successfully");
+  })
+  .catch((error: any) => {
+    console.log(error);
+    process.exit(1);
+  });
--- be/src/utils/index.ts
+++ be/src/utils/index.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Replace <password> with your actual MongoDB password
+
 mongoose
   .connect(
     "mongodb+srv://farhan:<password>@cluster0.t2nn1lv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
@@ -19,7 +19,7 @@ mongoose
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-// User registration route
+
 app.post("/register", async (req, res) => {
   try {
     const { user_id, email, name, password } = req.body;
@@ -32,12 +32,10 @@ app.post("/register", async (req, res) => {
   }
 });
 
-// Simple test route
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
-// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
